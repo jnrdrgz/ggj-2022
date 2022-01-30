f30b537916c477c580c581d111b4467c495e90a7
jnrdrgz <jnigrodriguez@gmail.com>
2022-01-30 03:53:04 -0300

    level2

--- Levels/Level2.gd
+++ Levels/Level2.gd
@@ -4,3 +4,28 @@ func _ready():
 	._ready()
 	print("level 2 constructors")
 	Sound.play("music")
+	#$ViewportContainer.pause_mode = PAUSE_MODE_PROCESS
+	get_tree().paused = true
+
+func _input(event):
+	if Input.is_action_just_pressed("play_slice_1"):
+		get_tree().paused = false
+		
+		print("pause")
+		
+func _process(delta):
+	if Input.is_action_just_pressed("play_slice_1"):
+		get_tree().paused = false
+		print("pause")
+		#$ViewportContainer.pause_mode = PAUSE_MODE_INHERIT
+		#$ViewportContainer2.pause_mode = PAUSE_MODE_PROCESS
+		#get_tree().paused = true
+	if Input.is_action_just_pressed("play_slice_2"):
+		get_tree().paused = false
+		$ViewportContainer.pause_mode = PAUSE_MODE_PROCESS
+		$ViewportContainer2.pause_mode = PAUSE_MODE_INHERIT
+		get_tree().paused = true
+
+
+func _on_ViewportContainer2_mouse_entered():
+	print("pause")
---
+++ Levels/Unpauser.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _process(delta):
+	if Input.is_action_just_pressed("play_slice_1"):
+		get_tree().paused = false
+		get_parent().get_node("ViewportContainer").pause_mode = PAUSE_MODE_PROCESS
+		get_parent().get_node("ViewportContainer2").pause_mode = PAUSE_MODE_INHERIT
+		get_tree().paused = true
+	if Input.is_action_just_pressed("play_slice_2"):
+		get_tree().paused = false
+		get_parent().get_node("ViewportContainer2").pause_mode = PAUSE_MODE_PROCESS
+		get_parent().get_node("ViewportContainer").pause_mode = PAUSE_MODE_INHERIT
+		get_tree().paused = true
